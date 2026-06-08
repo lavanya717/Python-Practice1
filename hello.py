dda658fdc95a62934317b26e5a854460e861be4b
lavanya717 <nadimintilavanya954@gmail.com>
2026-06-08 14:31:25 +0530

    Add files via upload
    Added hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+# print("i am learning python")
+# name = "lavanya"
+# age = "20"
+# college = "engineering"
+# print("my name is",name)
+# print("i am ",age, "years old")
+# print("i am a", college,"student")
+# a=10
+# b=9.5
+# c="lavanya"
+# d=False
+# print
